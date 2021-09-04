c8ff2ed9c16d0475cf192c45d617545fa1185d04
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-09-03 22:42:51 -0600

    UPDATE SWITCH
    Sé actualizó la sentencia switch para que puedan o no venir instrucciones dentro de los CASE´S y DEFAULT

---
+++ [OLC1]_CopyAnalyzer_201905554/src/AnalyzerJavascript/universal.java
@@ -0,0 +1,224 @@
+package AnalyzerJavascript;
+class universal{
+    //DECLARANDO VARIABLES ANY SIN PUNTO Y COMA
+    a=1
+    b=1.5
+    c='hola'
+    d="maria"
+    //DECLARANDO VARIABLES ANY CON PUNTO Y COMA
+    ax=1;
+    bx=1.5;
+    cx='hola';
+    dx="maria";
+    //FUNCION SIN PARAMETROS
+    funcion(){
+        //DECLARANDO VARIABLES VAR LET Y CONST CON PUNTO Y COMA
+        var aa=1;
+        var ba=1.25;
+        var ca='ah';
+        var da="hola";
+    
+        let aa=1;
+        let ba=1.25;
+        let ca='ah';
+        let da="hola";
+    
+        const aa=1;
+        const ba=1.25;
+        const ca='ah';
+        const da="hola";
+        //DECLARANDO VARIABLES VAR LET Y CONST SIN PUNTO Y COMA
+        var aa=1
+        var ba=1.25
+        var ca='ah'
+        var da="hola"
+    
+        let aa=1
+        let ba=1.25
+        let ca='ah'
+        let da="hola"
+    
+        const aa=1
+        const ba=1.25
+        const ca='ah'
+        const da="hola"
+        
+        //SENTENCIA IF SIMPLE CON IMPRESION
+        if(aa==5){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(aa!=5){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(aa<5){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(aa>5){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(aa<=5){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(aa>=5){
+            console.log(aa)
+            console.log(aa);
+        }
+        //SENTENCIA IF CON OPERADORES LOGICOS E IMPRESION
+        if(aa==5 && ba==4){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(aa!=5 || ba==4){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(!(aa<5) && ba==4){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(!(aa<5) || ba==4){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(ba==2 || !(aa<5)){
+            console.log(aa)
+            console.log(aa);
+        }
+        if(!(aa<5)){
+            console.log(aa)
+            console.log(aa);
+        }
+        //SENTENCIA IF ELSE SIMPLE
+        if(aa==5){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(aa!=5){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(aa<5){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(aa>5){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(aa<=5){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(aa>=5){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+         //SENTENCIA IF else CON OPERADORES LOGICOS E IMPRESION
+         if(aa==5 && ba==4){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(aa!=5 || ba==4){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(!(aa<5) && ba==4){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(!(aa<5) || ba==4){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(ba==2 || !(aa<5)){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        if(!(aa<5)){
+            console.log(aa)
+            console.log(aa);
+        }else{
+            console.log(ba)
+        }
+        //IF-ELSE IF SIMPLE
+        if(aa==5 && ba==4){
+            console.log(aa)
+            console.log(aa);
+        }else if(aa==3 && ba==4){
+            console.log(ba)
+        }
+        else if(aa==3 && ba==4){
+            console.log(hola)
+        }
+        //FOR SIMPLE CON IMPRESION
+        for(let i=0;i<4;i++){
+            console.log(forsimple)
+        }
+        //FOR SIMPLE CON DECLARACION ANTES
+        let i
+        for(i=0;i<4;i++){
+            console.log(forsimple)
+        }
+        //WHILE SIMPLE CON IMPRESION
+        while(aa==5){
+            console.log(whilesimple)
+        }
+        //WHILE CON OPERADORES LOGICOS
+        while(aa==5){
+            console.log(whilesimpleoperadorlocio)
+        }
+        //ciclos complejos
+        if(aa>5){
+            for(i=0;i<4;i++){
+                console.log(forsimple)
+                while(aa==5){
+                    console.log(whilesimpleoperadorlocio)
+                }
+                do{
+                    console.log(ba)
+                }while(aa<4){
+                    console.log(llegue)
+                }
+            }
+        }
+        switch(numero){
+            case 0: 
+            // instrucciones 
+            break; 
+            case 1: 
+            // instrucciones 
+            break; 
+            default: 
+            // instrucciones 
+           } 
+
+    }
+    
+    }
